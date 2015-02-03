bac8203b4e68d58efade57fef21bab3de8b70bfc
Sándor Csendes <shanedinozzo@gmail.com>
2015-02-03 15:48:24 +0100

    Initial commit

---
+++ files_folders.py
@@ -0,0 +1,51 @@
+__author__ = 'Shane DiNozzo'
+
+import os
+
+
+def getfoldersize(start_path):
+    total_size = 0
+    total_size_twodecimal = ""
+
+    for dirpath, dirnames, filenames in os.walk(start_path):
+        for f in filenames:
+            filepath = os.path.join(dirpath, f)
+            total_size += os.path.getsize(filepath)
+
+    if 1 <= len(str(total_size)) <= 3:
+        total_size_twodecimal = "%.2f" % total_size + " B"
+    elif 4 <= len(str(total_size)) <= 6:
+        total_size_in_kb = total_size / 1024
+        total_size_twodecimal = "%.2f" % total_size_in_kb + " KB"
+    elif 7 <= len(str(total_size)) <= 9:
+        mb = 1024 * 1024
+        total_size_in_mb = total_size / mb
+        total_size_twodecimal = "%.2f" % total_size_in_mb + " MB"
+    elif 10 <= len(str(total_size)) <= 12:
+        gb = 1024 * 1024 * 1024
+        total_size_in_gb = total_size / gb
+        total_size_twodecimal = "%.2f" % total_size_in_gb + " GB"
+    elif 13 <= len(str(total_size)) <= 15:
+        tb = 1024 * 1024 * 1024
+        total_size_in_tb = total_size / tb
+        total_size_twodecimal = "%.2f" % total_size_in_tb + " TB"
+    else:
+        print("The size of the folder is greater than TB!")
+
+    return total_size_twodecimal
+
+
+def getfilescount(start_path):
+    count = 0
+    for f in os.listdir(start_path):
+        if os.path.isfile(os.path.join(start_path, f)):
+            count += 1
+
+    return str(count) + " file(s)"
+
+
+def getfolderscount(start_path):
+    foldercount_array = [os.path.join(start_path, o) for o in os.listdir(start_path) if
+                         os.path.isdir(os.path.join(start_path, o))]
+
+    return str(foldercount_array.__len__()) + " folder(s)"
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='files_folders',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Shane DiNozzo',
+    author_email='shanedinozzo@gmail.com',
+    description='A little Python 3.4 module to get the size of a folder and get the count of the files and' +
+                ' subfolders in it'
+)
